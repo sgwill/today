3e7e50b4bf9434a7e7183a4b67e8f97dcd7717e9
Sam Williamson <sam.williamson@cqlcorp.com>
2012-09-10 10:42:06 -0400

    removed the crap

--- src/Web/Today.Web/Global.asax.cs
+++ src/Web/Today.Web/Global.asax.cs
@@ -16,9 +16,6 @@ namespace Today.Web
 	{
 		protected void Application_Start()
 		{
-			AreaRegistration.RegisterAllAreas();
-
-			WebApiConfig.Register(GlobalConfiguration.Configuration);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
